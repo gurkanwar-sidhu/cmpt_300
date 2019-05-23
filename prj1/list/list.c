002b1aeee7face7b393d3f251e49f055d948e73a
Gurkanwar Sidhu <gss28@sfu.ca>
2019-05-22 23:33:26 -0700

    list update

--- prj1/list/list.c
+++ prj1/list/list.c
@@ -1,15 +1,21 @@
 #include "list.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/*struct listStruct {
+	struct nodeStruct *head;
+	struct nodeStruct *tail;
+};*/
 /*
  * Allocate memory for a node of type struct nodeStruct and initialize
  * it with the value item. Return a pointer to the new node.
  */
 struct nodeStruct* List_createNode(int item){
 
-    struct nodeStruct* aNode = malloc(sizeof(struct nodeStruct));
-    aNode->item = item;
-    aNode->next = NULL;
-    return aNode;
+    struct nodeStruct* newNode = malloc(sizeof(struct nodeStruct));
+    newNode->item = item;
+    newNode->next = NULL;
+    return newNode;
 }
 
 
@@ -18,8 +24,9 @@ struct nodeStruct* List_createNode(int item){
  */
 
 void List_insertHead (struct nodeStruct **headRef, struct nodeStruct *node){
-    *headRef = node;
     
+	node->next = *headRef;
+	*headRef = node;
 }
 
 
@@ -27,12 +34,15 @@ void List_insertHead (struct nodeStruct **headRef, struct nodeStruct *node){
  * Insert node after the tail of the list.
  */
 void List_insertTail (struct nodeStruct **headRef, struct nodeStruct *node){
-    struct nodeStruct *temp = *headRef;
+    
+	struct nodeStruct *temp = malloc(sizeof(struct nodeStruct));
+	*temp = *headRef;
 
     while(temp->next != NULL){
         temp = temp->next;
     }
     temp->next = node;
+	node->next = NULL;
 }
 
 
@@ -41,17 +51,49 @@ void List_insertTail (struct nodeStruct **headRef, struct nodeStruct *node){
  * Return 0 if the list is empty, i.e., head == NULL
  */
 int List_countNodes (struct nodeStruct *head){
+
+	int count = 0;
     if(head == NULL){
-        return 0;
+        return count;
     }
-    return 0;
+	else{
+		struct nodeStruct *temp = malloc(sizeof(struct nodeStruct));
+		temp = head;
+
+    	while(temp->next != NULL){
+			count++;
+        	temp = temp->next;
+    	}
+    return count;
+	}
 }
 
 
 /*
  * Return the first node holding the value item, return NULL if none found
  */
-struct nodeStruct* List_findNode(struct nodeStruct *head, int item);
+struct nodeStruct* List_findNode(struct nodeStruct *head, int item){
+
+	if(head->item == item){
+		return &head;
+	}
+
+	struct nodeStruct *temp = malloc(sizeof(struct nodeStruct));
+	temp = head;
+
+	while( temp->item != item){
+		
+		if( temp->item == item){
+			return &temp;
+		}
+		
+		temp = temp->next;
+	}
+
+	if(temp->next == NULL){
+		return NULL;
+	}	
+}
 
 /*
  * Delete node from the list and free memory allocated to it.
