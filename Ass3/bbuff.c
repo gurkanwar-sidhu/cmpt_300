42b24c91368d54e5ff634c6ba65e3cac466170ab
sarbjot-14 <sarbjot_14@hotmail.com>
2019-06-27 17:59:15 -0700

    i dont understand why you cant pay attention

--- Ass3/bbuff.c
+++ Ass3/bbuff.c
@@ -2,40 +2,73 @@
 #define BBUFF_H
 
 #define BUFFER_SIZE 10
-
+#include <stdbool.h>
 // check : for testing delete these:
 #include <stdio.h> 
 #include <stdlib.h> 
+
+//delete these ^^^
+
 typedef struct  {
     int factory_number;
     double time_stamp_in_ms;
 } candy_t;
-//delete these ^^^
 
 void* theBuffer[BUFFER_SIZE]; 
 int front;
 int size;
+int tail;
 void bbuff_init(void){
 
     front= 0;
     size=0;
+   
 }
 void bbuff_blocking_insert(void* item){
+    if(size == 10){
+        printf("toooo big");
+    }
+    printf("inserting in %d\n", (front+size)%10);
+    theBuffer[(front+size)%10] = item;
     
-    theBuffer[size] = item;
     size++;
     //print all for testing
-    int itr = 0;
+    int itr = front;
+    int tempSize= 0;
     candy_t *candy;
-    while(itr != size){
+    if(size!=0){
+        while(tempSize <size){
+        itr = itr%10;
         candy  = theBuffer[itr];
         printf("print %d\n", candy->factory_number );
+        
+        if(candy->factory_number ==13){
+            break;
+        }
         itr++;
+        tempSize++;
+ 
+        
+        }
     }
-    printf("insert\n\n");
+    
+    //printf("insert\n\n");
 }
-void* bbuff_blocking_extract(void);
-_Bool bbuff_is_empty(void);
+void* bbuff_blocking_extract(void){
+    candy_t *candy = theBuffer[front];
+    front++;
+    front = front % 10;
+    size--;
+    return candy;
+}
+_Bool bbuff_is_empty(void){
+    if(size ==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 
 #endif
--- Ass3/candykids.c
+++ Ass3/candykids.c
@@ -26,7 +26,7 @@ int factories = 0;
 int kids = 0;
 int seconds = 0;
 
-int count = 0; //testing
+int myCount = 0; //testing
 //factory_number: tracks which factory thread produced the candy item
 // time_stamp_in_ms: tracks when the item was created. You can get the current number of milliseconds using the following function
 //check: must be linked through -lrt flag, What is -lrt flag?
@@ -48,7 +48,7 @@ typedef struct  {
 //inserting one candy
 void insertCandy() {
     candy_t *candy = malloc(sizeof (candy_t)); //check NEED TO FREE EVERY CANDY THEN ARRAY
-    candy->factory_number = count; //check fix these two
+    candy->factory_number = myCount; //check fix these two, with actual factory number and time
     candy->time_stamp_in_ms = 55;
     bbuff_blocking_insert(candy);
 }
@@ -77,18 +77,86 @@ int main(int argc, char *argv[]){
     		//Spawn the requested number of candy-factory threads. To each thread, pass it its factory number: 0 to (number of factories - 1).
 			//- Hint: Store the thread IDs in an array because you’ll need to join on them later.
 			//- Hint: Don’t pass each thread a reference to the same variable because as you change the variable’s value for the next thread, there’s no guaranty the previous thread will have read the previous value yet. You can use an array to have a different variable for each thread
-	
+	/* 
+	for every factory	
+		// Spawn thread
+		pthread_id daThreadId;
+		pthread_create(&daThreadId, ...)
+		//ask call dathread_function(void* arg)???? Inserting the candies
+		// Wait
+		sleep(...)
+
+		// Tell thread to stop itself, and then wait until it's done.
+		stop_thread = true;
+		pthread_join(daThreadID, NULL)
+*/
+
+
 	//check: i am not gonna do worry about threads right now. just gonna try to perfect the buffer. inserting deleting
 		//add a candies to the buffer
-	for(int i =0; i < 5; i++){
-		count++;
+	for(int i =0; i < 10; i++){ //testing...
+		myCount++;
 		insertCandy();
+		/* 
+		if(i%3 ==0 && i>0){
+			myPrint("extracting");
+			candy_t *ret_candy = bbuff_blocking_extract();
+			sprintf(sprintString, "returned candy is %d", ret_candy->factory_number);
+			myPrint(sprintString);
+		}
+		*/
 	}
+	myPrint("extracting");
+	candy_t *ret_candy = bbuff_blocking_extract();
+	sprintf(sprintString, "returned candy is %d", ret_candy->factory_number);
+	myPrint(sprintString);
+
+	myPrint("extracting");
+	ret_candy = bbuff_blocking_extract();
+	sprintf(sprintString, "returned candy is %d", ret_candy->factory_number);
+	myPrint(sprintString);
+
+	myPrint("extracting");
+	ret_candy = bbuff_blocking_extract();
+	sprintf(sprintString, "returned candy is %d", ret_candy->factory_number);
+	myPrint(sprintString);
+	myCount++;
+	insertCandy();
+	myCount++;
+	insertCandy();
+	
+
+	myCount++;
+	insertCandy();
+	myCount++;
+	insertCandy();
+	myCount++;
+	insertCandy();
 
 
     // 4.  Launch kid threads:
     		// Spawn the requested number of kid threads
 
+/*
+for every kid
+		// Spawn thread
+		pthread_id daThreadId;
+		pthread_create(&daThreadId, ...)
+		//ask call dathread_function(void* arg)???? Eat candies??
+		// Wait
+		sleep(...)
+
+		// Tell thread to stop itself, and then wait until it's done.
+		stop_thread = true;
+		pthread_join(daThreadID, NULL)
+		
+dathread_function
+Loop forever
+Extract a candy item from the bounded buffer.
+This will block until there is a candy item to extract.
+Process the item. Initially you may just want to printf() it to the screen; in the next section, you must add a statistics module that will track what candies have been eaten.
+Sleep for either 0 or 1 seconds (randomly selected). The kid threads are canceled from main() using pthread_cancel(). When this occurs, it is likely that the kid thread will be waiting on the semaphore in the bounded buffer. This should not cause problems.
+ */
     // 5.  Wait for requested time:
     		// In a loop, call sleep(1). Loop as many times as the “# Seconds” command line argument. Print the number of seconds running each time, such as “Time 3s” after the 3rd sleep. This shows time ticking away as your program executes.
 
